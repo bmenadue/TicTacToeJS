1c866a77ab3783e29d364fb43f7199d82e48b789
Bryce Menadue <bmenadue@innovia.com>
2020-02-28 16:20:56 -0600

    finished ai

--- script.js
+++ script.js
@@ -81,7 +81,8 @@ function emptySquare() {
 }
 
 function bestSpot() {
-  return emptySquare()[0];
+  //return emptySquare()[0];
+  return miniMax(origBoard, aiPlayer).index;
 }
 
 function checkTie() {
@@ -94,4 +95,55 @@ function checkTie() {
     return true;
   }
   return false;
+}
+
+function miniMax(newBoard, player) {
+  var availSpots = emptySquare(newBoard);
+
+  if (checkWin(newBoard, player)) {
+    return { score: -10 };
+  } else if (checkWin(newBoard, aiPlayer)) {
+    return { score: 10 };
+  } else if (availSpots.length === 0) {
+    return { score: 0 };
+  }
+
+  var moves = [];
+  for (var i = 0; i < availSpots.length; i++) {
+    var move = {};
+    move.index = newBoard[availSpots[i]];
+    newBoard[availSpots[i]] = player;
+
+    if (player == aiPlayer) {
+      var result = miniMax(newBoard, huPlayer);
+      move.score = result.score;
+    } else {
+      var result = miniMax(newBoard, aiPlayer);
+      move.score = result.score;
+    }
+
+    newBoard[availSpots[i]] = move.index;
+
+    moves.push(move);
+
+    var bestMove;
+    if (player === aiPlayer) {
+      var bestScore = -10000;
+      for (var i = 0; i < moves.length; i++) {
+        if (moves[i].score > bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    } else {
+      var bestScore = 10000;
+      for (var i = 0; i < moves.length; i++) {
+        if (moves[i].score < bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    }
+  }
+  return moves[bestMove];
 }
