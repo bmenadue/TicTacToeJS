83a8629bf8eecda9ec7c88c999cc5628a0a6073a
Bryce Menadue <bmenadue@innovia.com>
2020-02-28 15:28:44 -0600

    built checkWin and gameOver

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const huPlayer = "O";
 const aiPlayer = "X";
 
 const winCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+  [0, 1, 2], //0
+  [3, 4, 5], //1
+  [6, 7, 8], //2
+  [0, 3, 6], //3
+  [1, 4, 7], //4
+  [2, 5, 8], //5
+  [0, 4, 8], //6
+  [2, 4, 6] //7
 ];
 
 const cells = document.querySelectorAll(".cell");
@@ -36,4 +36,31 @@ function turnClick(square) {
 function turn(squareId, player) {
   origBoard[squareId] = player;
   document.getElementById(squareId).innerText = player;
+  let gameWon = checkWin(origBoard, player);
+  if (gameWon) gameOver(gameWon);
+}
+
+function checkWin(board, player) {
+  let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
+
+  let gameWon = null;
+
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every(elem => plays.indexOf(elem) > -1)) {
+      gameWon = { index: index, player: player };
+      break;
+    }
+  }
+  return gameWon;
+}
+
+function gameOver(gameWon) {
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor =
+      gameWon.player == huPlayer ? "blue" : "red";
+  }
+
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener("click", turnClick, false);
+  }
 }
